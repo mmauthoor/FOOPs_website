0d1ee3a08622203dc85af8050372d2234a0390b5
moya <moyamauthoor@outlook.com>
2021-12-31 14:26:54 +1100

    Add additional view pages and progressed drowdown navbar

--- main.rb
+++ main.rb
@@ -4,3 +4,19 @@ require "sinatra/reloader"
 get "/" do
     erb(:index)
 end
+
+get "/about" do
+    erb(:about)
+end
+
+get "/join" do
+    erb(:join)
+end
+
+get "/projects" do
+    erb(:projects)
+end
+
+get "/resources" do
+    erb(:resources)
+end
