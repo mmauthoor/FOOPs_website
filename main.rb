b2c590d3609f54aa267f45b4f9cdf420463d3fca
moya <moyamauthoor@outlook.com>
2022-01-04 17:45:59 +1100

    Update put /wbdates route to allow dates to be updated all at the same time rather than individually

--- main.rb
+++ main.rb
@@ -39,8 +39,8 @@ get "/resources" do
 end
 
 # should only be able to access this route via login page for site admin
-# In database, change 'name' to 'month'
 get "/wbdates" do
+    # would have login check here verify user
     sql = "SELECT * FROM dates ORDER BY id;"
     conn = PG.connect( dbname: 'foopswbdates')
     result = conn.exec(sql)
@@ -51,27 +51,43 @@ get "/wbdates" do
     })
 end
 
-get "/wbdates/:id/edit" do
+# No longer require these routes - are designed to edit single date only. 
+# get "/wbdates/:id/edit" do
     
-    sql = "SELECT * FROM dates WHERE id = #{params["id"]};"
-    conn = PG.connect( dbname: 'foopswbdates')
-    result = conn.exec(sql)
-    conn.close
+#     sql = "SELECT * FROM dates WHERE id = #{params["id"]};"
+#     conn = PG.connect( dbname: 'foopswbdates')
+#     result = conn.exec(sql)
+#     conn.close
 
-    date = result[0]
-    erb(:edit_date, locals: {
-        date: date
-    })
-end
+#     date = result[0]
+#     erb(:edit_date, locals: {
+#         date: date
+#     })
+# end
 
-put "/wbdates/:id" do
+# put "/wbdates/:id" do
 
-    date = params["date"]
-    sql = "UPDATE dates SET date = '#{params["date"]}' WHERE id = #{params["id"]};"
+#     date = params["date"]
+#     sql = "UPDATE dates SET date = '#{params["date"]}' WHERE id = #{params["id"]};"
+#     conn = PG.connect(dbname: 'foopswbdates')
+#     conn.exec(sql)
+#     conn.close
+
+#     redirect "/wbdates"
+    
+# end
+
+put "/wbdates" do
     conn = PG.connect(dbname: 'foopswbdates')
-    conn.exec(sql)
-    conn.close
+    
+    params.each do | id, date |
+        if id.to_i > 0
+            sql = "UPDATE dates SET date = '#{date}' WHERE id = #{id};"
+            conn.exec(sql)
+        end
+    end
 
-    redirect "/wbdates"
+    conn.close
+    redirect "/"
     
 end
